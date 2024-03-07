b48f41bfe02fc53466b4526057250c2f8e9056b7
Naman Khater <naman22khater@gmail.com>
2024-03-08 00:10:48 +0530

    more props

--- textfield/src/TextField.js
+++ textfield/src/TextField.js
@@ -5,12 +5,14 @@ const generateId = (prefix) => {
     return `${prefix}-${Math.random().toString(36).substr(2, 9)}`;
 };
 
-const TextFieldPlugin = ({ label, fullWidth }) => {
+const TextFieldPlugin = ({ label, fullWidth, error, helperText }) => {
     return (
         <TextField 
             id={generateId("textfield")}
             label={label}
             fullWidth={fullWidth}
+            error={error}
+            helperText={helperText}
         />
     );
 };  
